5d505050a1b9e80d069eaa1efa242cbd86d007e8
goatbotz <goatbotz123@gmail.com>
2024-10-07 23:30:39 +0800

    Update and rename account.txt to appstate.js

---
+++ appstate.js
@@ -0,0 +1,72 @@
+[{
+	"key": "datr",
+	"value": "G-EBZ0sI1CEF8GWD20sFVesX",
+	"path": "/",
+	"hostOnly": false,
+	"domain": ".facebook.com",
+	"creation": "2024-10-07T15:29:28.010Z",
+	"lastAccessed": "2024-10-07T15:29:28.010Z"
+	},
+	{
+	"key": "sb",
+	"value": "G-EBZ4RKfCdv9OFdOOJ5XZ8Q",
+	"path": "/",
+	"hostOnly": false,
+	"domain": ".facebook.com",
+	"creation": "2024-10-07T15:29:28.010Z",
+	"lastAccessed": "2024-10-07T15:29:28.010Z"
+	},
+	{
+	"key": "m_pixel_ratio",
+	"value": "2",
+	"path": "/",
+	"hostOnly": false,
+	"domain": ".facebook.com",
+	"creation": "2024-10-07T15:29:28.010Z",
+	"lastAccessed": "2024-10-07T15:29:28.010Z"
+	},
+	{
+	"key": "locale",
+	"value": "en_US",
+	"path": "/",
+	"hostOnly": false,
+	"domain": ".facebook.com",
+	"creation": "2024-10-07T15:29:28.010Z",
+	"lastAccessed": "2024-10-07T15:29:28.010Z"
+	},
+	{
+	"key": "c_user",
+	"value": "61566832856662",
+	"path": "/",
+	"hostOnly": false,
+	"domain": ".facebook.com",
+	"creation": "2024-10-07T15:29:28.010Z",
+	"lastAccessed": "2024-10-07T15:29:28.010Z"
+	},
+	{
+	"key": "fr",
+	"value": "03cbhqVmzG81xsIkj.AWWzvXtsisCKWgMk7w5gFotvhag.BnAeEb..AAA.0.0.BnA_5K.AWUr8oktMqI",
+	"path": "/",
+	"hostOnly": false,
+	"domain": ".facebook.com",
+	"creation": "2024-10-07T15:29:28.010Z",
+	"lastAccessed": "2024-10-07T15:29:28.010Z"
+	},
+	{
+	"key": "xs",
+	"value": "50%3APDYV1nDLB7jUKA%3A2%3A1728314954%3A-1%3A-1",
+	"path": "/",
+	"hostOnly": false,
+	"domain": ".facebook.com",
+	"creation": "2024-10-07T15:29:28.010Z",
+	"lastAccessed": "2024-10-07T15:29:28.010Z"
+	},
+	{
+	"key": "wl_cbv",
+	"value": "v2%3Bclient_version%3A2642%3Btimestamp%3A1728314957",
+	"path": "/",
+	"hostOnly": false,
+	"domain": ".facebook.com",
+	"creation": "2024-10-07T15:29:28.010Z",
+	"lastAccessed": "2024-10-07T15:29:28.010Z"
+	}]
